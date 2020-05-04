af5dd981f043c5aa55bc0c6dcf1ce7265a0df614
ChevalGlass <darkhand16@gmail.com>
2020-05-04 01:22:09 -0700

    initial testing to cache the player names
    began implementing a "cache" by saving a json string to disk.
    Python datetime isn't as straight forward as I had hoped.

--- uuid_to_player_name.py
+++ uuid_to_player_name.py
@@ -12,6 +12,7 @@
 import os
 import json
 import requests
+import datetime
 
 def requestPlayerNameFromUUID(uuid):
 	response = requests.get('https://api.mojang.com/user/profiles/'+uuid.replace('-', '')+'/names')
@@ -22,5 +23,38 @@ def getPlayerNamesFromUuids(uuids):
 
 	for player in uuids:
 		player_names[player] = requestPlayerNameFromUUID(player)
+
+	return player_names
+
+# Not fully implemented. Learning python datetime.
+def getPlayerNamesFromUuidsWithCache(uuids):
+	player_names = {}
+
+	# Check for and load the json cache.
+	if os.path.exists('player_cache.json'):
+		with open('player_cache.json') as player_cache:
+			player_names = json.loads(player_cache.read())["players"]
+
+		# Check if the cache was recent. (within a ~ 1 day)
+
+		# If it was, make sure all the uuids requested are in the cache.
+
+		# Request any uuid's that are not in the cache.
+
+	else: # If no cache exists, prep one.
+		# Set up the framework with now as the cache date and an entry player dictionary.
+		player_names = { "cache_date" : datetime.datetime.now().isoformat(), "players" : {} }
+
+		for player in uuids:
+			player_names["players"][player] = requestPlayerNameFromUUID(player)
+
+		print('cache_date', player_cache["cache_date"])
+		#if player_names["cache_date"]
+
+		# with open('player_cache.json', 'w') as player_cache:
+			# json.dump(player_names, player_cache)
+
+#	for player in uuids:
+#		player_names[player] = requestPlayerNameFromUUID(player)
 
 	return player_names
